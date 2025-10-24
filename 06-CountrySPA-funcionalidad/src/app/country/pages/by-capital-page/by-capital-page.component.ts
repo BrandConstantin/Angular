edd8d96d6263019393b15dbe387bf877e2f73f8a
BrandConstantin <constantin.brindu@gmail.com>
2025-10-25 01:08:54 +0200

    06-CountrySPA-funcionalidad

---
+++ 06-CountrySPA-funcionalidad/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CountrySearchInputComponent } from '../../components/search-input.component/search-input.component';
+import { CountryListComponent } from '../../components/country-list.component/country-list.component';
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [CountrySearchInputComponent, CountryListComponent],
+  templateUrl: './by-capital-page.component.html',
+})
+export class ByCapitalPageComponent {
+  onSearchCapital( capital: string ) {
+    console.log({ capital });
+  }
+}
