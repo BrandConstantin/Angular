bdc06213ead55bd48fb81f0c496f752598dbb91b
BrandConstantin <constantin.brindu@gmail.com>
2025-11-12 07:34:06 +0100

    formularios reactivos

---
+++ 10-FormulariosReactivos_A/src/app/country/pages/country-page/country-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-page.html',
+})
+export class CountryPageComponent { }
