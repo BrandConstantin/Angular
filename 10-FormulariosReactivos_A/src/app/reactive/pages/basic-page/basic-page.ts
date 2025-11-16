a786d0437e77cfeb279a0ad71c3d63ddbb564629
BrandConstantin <constantin.brindu@gmail.com>
2025-11-16 16:57:19 +0100

    validadores

--- 10-FormulariosReactivos_A/src/app/reactive/pages/basic-page/basic-page.ts
+++ 10-FormulariosReactivos_A/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,9 +10,20 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BasicPage {
   // forma muy tediosa
+  /*
   myForm = new FormGroup({
     name: new FormControl(''),
     price: new FormControl(0),
     inStorage: new FormControl(0)
+  })
+  */
+
+  // con formBuilder
+  private fb = inject(FormBuilder);
+  myForm = this.fb.group({
+    //name: ['', /** validadores sincronos */, /** validadores asincronos */],
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
   })
 }
--- README.md
+++ README.md
@@ -1285,8 +1285,25 @@ myForm = new FormGroup({
 ```
 
 ## Form Builder
+```
+private fb = inject(FormBuilder);
+myForm = this.fb.group({
+  name: [''],
+  price: [0],
+  inStorage: [0]
+})
+```
 
-## FormControls
+## Validadores
+```
+private fb = inject(FormBuilder);
+myForm = this.fb.group({
+  //name: ['', /** validadores sincronos */, /** validadores asincronos */],
+  name: ['', [Validators.required, Validators.minLength(3)], []],
+  price: [0, [Validators.required, Validators.min(10)]],
+  inStorage: [0, [Validators.required, Validators.min(0)]]
+})
+```
 
 ## FormArrays
 
