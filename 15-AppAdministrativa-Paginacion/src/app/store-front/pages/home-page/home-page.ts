26cdd503a14c39b3a17021b2d3c9f31907f2629a
BrandConstantin <constantin.brindu@gmail.com>
2025-12-14 21:18:20 +0100

    servicio de paginación

--- 15-AppAdministrativa-Paginacion/src/app/store-front/pages/home-page/home-page.ts
+++ 15-AppAdministrativa-Paginacion/src/app/store-front/pages/home-page/home-page.ts
@@ -1,10 +1,9 @@
 import { ProductCard } from '@/products/components/product-card/product-card';
 import { ProductsService } from '@/products/services/product.service';
 import { Pagination } from '@/shared/components/pagination/pagination';
+import { PaginationService } from '@/shared/components/pagination/pagination.service';
 import { Component, inject } from '@angular/core';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
-import { map, pipe } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-page',
@@ -13,18 +12,21 @@ import { map, pipe } from 'rxjs';
 })
 export class HomePage { 
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
-  activatedRoute = inject(ActivatedRoute);
-  currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(
-    map((params) => (params.get('page') ? +params.get('page')! : 1)),
-    map((page) => (isNaN(page) || page < 1 ? 1 : page))
-   ),
-   { initialValue: 1 }
-  );
+  // activatedRoute = inject(ActivatedRoute);
+  // currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(
+  //   map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //   map((page) => (isNaN(page) || page < 1 ? 1 : page))
+  //  ),
+  //  { initialValue: 1 }
+  // );
+
+  currentPageService = this.paginationService.currentPage;
 
   // Usando rxResource, ha cambiado de request por params y loader por stream a partir de Angular 20
   productsResource = rxResource({
-    params: () => ({ page : this.currentPage() }),
+    params: () => ({ page : this.currentPageService() }),
     stream: ({ params }) => {
       return this.productsService.getProducts({
         offset: (params.page - 1) * 8,
