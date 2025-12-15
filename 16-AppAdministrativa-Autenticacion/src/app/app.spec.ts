71b87cc28eb44a634b51e46de6d7cfd147553d4d
BrandConstantin <constantin.brindu@gmail.com>
2025-12-15 07:35:14 +0100

    rutas y páginas de autenticación

--- 16-AppAdministrativa-Autenticacion/src/app/app.spec.ts
+++ 16-AppAdministrativa-Autenticacion/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 15-AppAdministrativa-Paginacion');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 16-AppAdministrativa-Autenticacion');
   });
 });
--- 16-AppAdministrativa-Autenticacion/src/app/app.ts
+++ 16-AppAdministrativa-Autenticacion/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('15-AppAdministrativa-Paginacion');
+  protected readonly title = signal('16-AppAdministrativa-Autenticacion');
 }
