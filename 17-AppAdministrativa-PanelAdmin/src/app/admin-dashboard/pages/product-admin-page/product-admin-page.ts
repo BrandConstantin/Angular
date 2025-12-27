3e7897ecee821748d8b05c16d0e05ed58a1cfe33
BrandConstantin <constantin.brindu@gmail.com>
2025-12-27 09:36:49 +0100

    rutas y guard para panel administrativo

---
+++ 17-AppAdministrativa-PanelAdmin/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-admin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-admin-page.html',
+})
+export class ProductAdminPage { }
---
+++ 17-AppAdministrativa-PanelAdmin/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-admin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-admin-page.html',
+})
+export class ProductsAdminPage { }
