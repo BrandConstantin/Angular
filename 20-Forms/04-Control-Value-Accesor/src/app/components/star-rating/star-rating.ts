c9e8320845017731232950c8cd5a49bb753b3a42
BrandConstantin <constantin.brindu@gmail.com>
2026-05-03 13:51:25 +0200

    add star rating component

--- 20-Forms/04-Control-Value-Accesor/src/app/components/star-rating/star-rating.ts
+++ 20-Forms/04-Control-Value-Accesor/src/app/components/star-rating/star-rating.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, input, model, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'star-rating',
   imports: [],
   templateUrl: './star-rating.html',
   styleUrl: './star-rating.css',
+  host: {
+    '[class.disabled]': 'isDisabled()',
+    'mouseleave': 'handleMouseLeave()'
+  }
 })
-export class StarRating {
+export class StarRating implements OnInit {
+  stars = input.required<boolean[], number>({transform: gettingStars}); // input para recibir el número de estrellas
+  rating = model<number>(0); // modelo para almacenar la calificación actual
+  hoveredIndex = signal<number>(-1); // índice de la estrella actualmente hoverada
+  isDisabled = input<boolean>(false, {alias: 'disabled'}); // input para deshabilitar la calificación
 
+  ngOnInit(): void {
+    //this.stars.set(Array(this.starCount()).fill(false));
+  }
+
+  rate(rating: number) {
+    if(this.isDisabled()) return; // si el componente está deshabilitado, no se permite calificar
+    this.rating.set(rating); // actualiza la calificación actual
+  }
+
+  handleMouseOver(index: number) {
+    this.hoveredIndex.set(index); // actualiza el índice de la estrella hoverada
+  }
+  
+  handleMouseLeave() {
+    this.hoveredIndex.set(-1); // restablece el índice de la estrella hoverada
+  }
+}
+
+function gettingStars(value: number): boolean[] {
+  return Array(value).fill(false);
 }
--- 20-Forms/04-Control-Value-Accesor/src/app/views/value-accesor/value-accesor.ts
+++ 20-Forms/04-Control-Value-Accesor/src/app/views/value-accesor/value-accesor.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { StarRating } from '../../components/star-rating/star-rating';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
@@ -10,4 +10,16 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class ValueAccesor {
   starRatingControl = new FormControl(3); // valor inicial de 3 estrellas
+
+  counter = signal(4); // contador para agregar estrellas
+
+  adding() {
+    this.counter.update((count) => count + 1); // incrementa el contador
+    this.starRatingControl.setValue(this.counter()); // actualiza el valor del control con el nuevo contador
+  }
+
+  ratingChange(newRating: number) {
+    console.log('Nueva calificación:', newRating);
+    this.starRatingControl.setValue(newRating); // actualiza el valor del control con la nueva calificación
+  }
 }
